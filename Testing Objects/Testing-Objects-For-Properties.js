4f261aef5b92277cd25427e6c85ee0822e9d26db
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-06-29 21:31:58 +0300

    Update Testing-Objects-For-Properties.js

--- Testing Objects/Testing-Objects-For-Properties.js
+++ Testing Objects/Testing-Objects-For-Properties.js
@@ -1,5 +1,4 @@
 function checkObj(obj, checkProp) {
-  // Only change code below this line
   const myObj = obj;
 
   if (myObj.hasOwnProperty(checkProp)) {
@@ -7,7 +6,6 @@ function checkObj(obj, checkProp) {
   else {
     return "Not Found";
   }
-  // Only change code above this line
 }
 
 console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "bed"))
